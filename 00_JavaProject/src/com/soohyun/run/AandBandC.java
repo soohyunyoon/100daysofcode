09b0ca7b570ef7b0ee7bf0be27a409e32db53299
soohyun yoon <59842439+soohyunyoon@users.noreply.github.com>
2020-07-09 23:05:33 +0900

    Update AandBandC.java
    Coding Test

--- 00_JavaProject/src/com/soohyun/run/AandBandC.java
+++ 00_JavaProject/src/com/soohyun/run/AandBandC.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class AandBandC {
 	public static void main(String[] args) {
 	 Scanner sc = new Scanner(System.in);
-     int A = sc.nextInt();
-     int B = sc.nextInt();
-     int C = sc.nextInt();
-     
-     System.out.println((A+B)%C);
-     System.out.println(((A%C) + (B%C))%C);
-     System.out.println((A*B)%C);
-     System.out.println(((A%C) * (B%C))%C);
+	     int A = sc.nextInt();
+	     int B = sc.nextInt();
+	     int C = sc.nextInt();
+
+	     System.out.println((A+B)%C);
+	     System.out.println(((A%C) + (B%C))%C);
+	     System.out.println((A*B)%C);
+	     System.out.println(((A%C) * (B%C))%C);
 
 	}
 }

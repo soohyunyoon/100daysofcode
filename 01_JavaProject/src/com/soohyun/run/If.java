766a92125d207a992d97ffcef1e419a8594cab7d
zozzo <zozzo@DESKTOP-J3LRVJP>
2020-07-09 23:24:43 +0900

    Coding Test

--- 01_JavaProject/src/com/soohyun/run/If.java
+++ 01_JavaProject/src/com/soohyun/run/If.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class If {
 
 	public static void main(String[] args) {   
-	
+		// 숫자 입력받기
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
+        int A = sc.nextInt(); 
         int B = sc.nextInt();
         
-        if (A > B) {
+        if (A > B) { // A가 B보다 클 경우
             System.out.println(">");
-        }else if (A < B) {
+        }else if (A < B) { // B가 A보다 클 경우
             System.out.println("<");
-        }else if (A == B) {
+        }else if (A == B) {// A와 B가 같을 경우
             System.out.println("==");
         }
     }
--- 01_JavaProject/src/com/soohyun/run/Score.java
+++ 01_JavaProject/src/com/soohyun/run/Score.java
@@ -8,13 +8,13 @@ public class Score {
 		Scanner sc = new Scanner(System.in);
 		int score = sc.nextInt();
     
-	    if(score >= 90) {
+	    if(score >= 90) { // 점수가 90점 이상
 	        System.out.println("A");
-	    }else if (score >= 80) {
+	    }else if (score >= 80) { // 점수가 80점 이상
 	        System.out.println("B");
-	    }else if (score >= 70) {
+	    }else if (score >= 70) { // 점수가 70점 이상
 	        System.out.println("C");
-	    }else if (score >= 60) {
+	    }else if (score >= 60) { // 점수가 60점 이상
 	         System.out.println("D");
 	    }else {
 	         System.out.println("F");

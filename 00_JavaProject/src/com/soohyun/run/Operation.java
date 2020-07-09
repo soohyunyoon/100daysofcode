90ff17fe25cfb9d1898567b8dfdd4c09e3d592a1
soohyun <zozzo@DESKTOP-J3LRVJP>
2020-07-09 22:04:53 +0900

    Coding Test

---
+++ 00_JavaProject/src/com/soohyun/run/Operation.java
@@ -0,0 +1,18 @@
+package com.soohyun.run;
+
+import java.util.Scanner;
+
+public class Operation {
+
+	   public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int A = sc.nextInt();
+	        int B = sc.nextInt();
+	        System.out.println(A + B);
+	        System.out.println(A - B);
+	        System.out.println(A * B);
+	        System.out.println(A / B);
+	        System.out.println(A % B);
+	    }
+
+}

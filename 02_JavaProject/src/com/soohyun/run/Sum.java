b1bed6c3d1ee0e1cd8835aae4febe671921e64f4
zozzo <zozzo@DESKTOP-J3LRVJP>
2020-07-17 23:24:27 +0900

    Coding Test

---
+++ 02_JavaProject/src/com/soohyun/run/Sum.java
@@ -0,0 +1,17 @@
+package com.soohyun.run;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	    public static void main(String[] args) {
+	         Scanner sc = new Scanner(System.in);
+	        int num = sc.nextInt();
+	        
+	        int sum = 0;
+	        for(int i=0; i<=num; i++) {
+	        	sum = sum + i;
+	        }
+	        System.out.println(sum);
+	    }
+}

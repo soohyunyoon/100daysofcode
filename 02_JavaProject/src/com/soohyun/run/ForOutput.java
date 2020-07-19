e824d616a16e3353195923e7738451f94c6174cb
soohyun yoon <59842439+soohyunyoon@users.noreply.github.com>
2020-07-19 23:22:43 +0900

    Add files via upload
    Coding Test

---
+++ 02_JavaProject/src/com/soohyun/run/ForOutput.java
@@ -0,0 +1,16 @@
+package com.soohyun.run;
+
+import java.util.Scanner;
+
+public class ForOutput {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        for(int i=1; i<=N; i++) {
+            System.out.println(i);
+	}
+
+}
+}

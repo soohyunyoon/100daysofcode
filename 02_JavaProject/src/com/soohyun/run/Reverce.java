2ddfe111ab40ab54e6ce6a6d188e98a02a530044
soohyun yoon <59842439+soohyunyoon@users.noreply.github.com>
2020-07-20 21:06:35 +0900

    Add files via upload
    Coding Test

---
+++ 02_JavaProject/src/com/soohyun/run/Reverce.java
@@ -0,0 +1,15 @@
+package com.soohyun.run;
+
+import java.util.Scanner;
+
+public class Reverce {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+	        int num = sc.nextInt();
+	        for(int i=num; i>0; i--) {
+	            System.out.println(i);
+	        }
+	}
+
+}

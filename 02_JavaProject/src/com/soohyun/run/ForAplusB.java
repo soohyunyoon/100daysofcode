6b25644f73591c3dc09aaf833afb863f3ae51e1e
soohyun yoon <59842439+soohyunyoon@users.noreply.github.com>
2020-07-22 22:55:49 +0900

    Add files via upload
    Coding Test

---
+++ 02_JavaProject/src/com/soohyun/run/ForAplusB.java
@@ -0,0 +1,21 @@
+package com.soohyun.run;
+
+import java.util.Scanner;
+
+public class ForAplusB {
+
+	public static void main(String[] args) {
+				
+		    Scanner sc = new Scanner(System.in);
+		    	int T = sc.nextInt();
+		          
+		            
+		        for(int i=1; i<=T; i++) {
+		            int A = sc.nextInt();
+		            int B = sc.nextInt();
+		            System.out.println("Case #" + i + ": " + (A + B));
+		        }
+	}
+	
+
+}

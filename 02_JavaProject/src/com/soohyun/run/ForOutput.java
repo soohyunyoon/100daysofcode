5ff6dcb93dcdfee56f0827c3506a18459e8145ea
soohyun yoon <shyuny58@google.com>
2020-07-27 17:44:18 +0900

    coding Test

--- 02_JavaProject/src/com/soohyun/run/ForOutput.java
+++ 02_JavaProject/src/com/soohyun/run/ForOutput.java
@@ -3,13 +3,14 @@ package com.soohyun.run;
 import java.util.Scanner;
 
 public class ForOutput {
-
+	// output
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         
         for(int i=1; i<=N; i++) {
             System.out.println(i);
+            
 	}
 
 }

52d516939d131a41cdaa145b1b4f30564e81cbd2
soohyun yoon <59842439+soohyunyoon@users.noreply.github.com>
2020-09-10 23:38:23 +0900

    Update Practice1.java
    update

--- 04_JavaProject/src/com/soohyun/run/Practice1.java
+++ 04_JavaProject/src/com/soohyun/run/Practice1.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Practice1 {
 
 	public static void main(String[] args) {
+		
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int num1 = sc.nextInt();
@@ -13,18 +15,25 @@ public class Practice1 {
 		int num4 = sc.nextInt();
 		int num5 = sc.nextInt();
 		
-		if(num1<40) {
-			num1 = 40;
-		}else if(num2<40) {
-			num2 = 40;
-		}else if(num3<40){
-			num3 = 40;
-		}else if(num4<40) {
-			num4 = 40;
-		}else if(num5<40) {
-			num5 = 40;
+		for(int i=0; i<5; i++) {
+			if(num1<40) {
+				num1 = 40;
+			}else if(num2<40) {
+				num2 = 40;
+			}else if(num3<40) {
+				num3 = 40;
+			}else if(num4<40) {
+				num4 = 40;
+			}else if(num5<40) {
+				num5 = 40;
+			}
 		}
-
+		int sum = num1 + num2 + num3 + num4 + num5;
+		
+		int avg = sum / 5;
+		System.out.println(avg);
+		
+		
 	}
 
 }
